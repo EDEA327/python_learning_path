06a25b8121ecc65cdb7eb3efd78ebdf8e6ae0cdd
Erick Escobar <edeajob27@gmail.com>
2022-08-22 11:09:47 -0500

    Creacion de votingBooth

--- python_POO/abstraction.py
+++ python_POO/abstraction.py
@@ -1,20 +1,22 @@
 class Washing_machine:
     def __init__(self):
         pass
-    def washing(self,temp = "Hot"):
+    def washing(self,temp = "hot"):
         self._fill_water_tank(temp)
         self._add_soap()
         self._wash()
         self._spin_dry()
     def _fill_water_tank(self,temp):
-        print(f'Filling tank with water {temp}')
+        print(f'Filling tank with {temp} water ')
     def _add_soap(self):
         print(f'Adding SOAP')
     def _wash(self):
-        print(f'Washing')
+        print(f'Washing clothes')
     def _spin_dry(self):
-        print(f'Spinning dry')
+        print(f'Spinning dry clothes')
 
 if __name__ == '__main__':
     washer = Washing_machine()
+    washer.washing()
+
 
