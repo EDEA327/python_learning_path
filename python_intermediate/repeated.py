96677791adad80e221f941adb579c6f30d321cfe
Erick Escobar <edeajob27@gmail.com>
2022-08-22 18:37:45 -0500

    pequeños ccambios

--- python_intermediate/repeated.py
+++ python_intermediate/repeated.py
@@ -6,16 +6,15 @@ def quit_repeated(my_list):
 
 def run():
     elements = []
-    elements_length = input(" How many elements? : ")
-    assert elements_length.isnumeric(),"Given number of elements must be a number"
-    if int(elements_length) <= 0:
+    elements_length = int(input(" How many elements? : "))
+    if elements_length <= 0 :
         raise ValueError("You must provide at least one element")
 
     for i in range(1,elements_length+1):
         element = input(f" Type your {i} element for the  list: ")
         elements.append(element)
 
-    print("elements = ",elements)
-    print(f"elements without repeated elements:{quit_repeated(elements)} ")
+    print("Original list = ",elements)
+    print(f"List without repeated elements:{quit_repeated(elements)} ")
 if __name__ == '__main__':
     run()
