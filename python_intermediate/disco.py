8fc566a6fcb2087134663654f5c3116cdcdd818e
Erick Escobar <edeajob27@gmail.com>
2022-08-22 18:41:58 -0500

    5comentarios

--- python_intermediate/disco.py
+++ python_intermediate/disco.py
@@ -3,8 +3,6 @@ def run():
     age=int(input("Por favor ingresa tu edad: "))
     if age < 18 :
         print("No se acepta el ingreso de menores de edad")
-    #! Si no le ponemos el menor de 50 nos puede pasar que ingresen un numero mayor de 50 y  se cumplirian las 2 condiciones
-    #! de abajo.
     elif age >= 50 :
         print("Bienvenido, algunas cervezas no estan a la venta para adultos mayores")
     elif age >= 18 :
