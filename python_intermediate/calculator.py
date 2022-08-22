3260d3237394a4bca2937d7b939ccba7ad88be02
Erick Escobar <edeajob27@gmail.com>
2022-08-22 17:45:06 -0500

    Algunas mejoras

--- python_intermediate/calculator.py
+++ python_intermediate/calculator.py
@@ -9,23 +9,27 @@ def run():
     3-Multiplication
     4-Division
     Plese select an option:  """
-    option = int(input(menu))
-    num1 = input("Type your first number: ")
-    assert num1.isnumeric()
-    num2 = input("Type your second number: ")
-    assert num2.isnumeric()
+
+    try:
+        option = int(input(menu))
+        num1 = input("Type your first number: ")
+        assert num1.isnumeric()
+        num2 = input("Type your second number: ")
+        assert num2.isnumeric()
     # Para evitar problemas con las operaciones
-    num1 = float(num1)
-    num2 = float(num2)
+        num1 = float(num1)
+        num2 = float(num2)
     # Ejececucion según lo escogido
-    if option == 1:
-        print(f" The addition  is {addition(num1,num2)}")
-    elif option == 2:
-        print(f"The subtraction is {subtraction(num1,num2)}")
-    elif option == 3:
-        print(f"The multiplication is {multiplication(num1,num2)}")
-    elif option == 4:
-        print(f"The division is {division(num1,num2)}")
+        if option == 1:
+            print(f"{num1} + {num2} = {addition(num1,num2)}")
+        elif option == 2:
+            print(f"{num1} - {num2}  = {subtraction(num1,num2)}")
+        elif option == 3:
+            print(f"{num1} * {num2} ={multiplication(num1,num2)}")
+        elif option == 4:
+            print(f"{num1} / {num2} {division(num1,num2)}")
+    except ValueError:
+        print("Type a valid option number")
 
 if __name__ == '__main__':
     run()
