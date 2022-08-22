9b44ba21bcd48ffa156b9d533ff90259d838f2b9
Erick Escobar <edeajob27@gmail.com>
2022-08-22 18:41:16 -0500

    Mejora de sintaxis

--- python_intermediate/disco.py
+++ python_intermediate/disco.py
@@ -5,10 +5,11 @@ def run():
         print("No se acepta el ingreso de menores de edad")
     #! Si no le ponemos el menor de 50 nos puede pasar que ingresen un numero mayor de 50 y  se cumplirian las 2 condiciones
     #! de abajo.
-    elif age >= 18 and age <= 50 :
-        print("Bienvenido puede entrar")
-    elif age > 50 :
+    elif age >= 50 :
         print("Bienvenido, algunas cervezas no estan a la venta para adultos mayores")
+    elif age >= 18 :
+        print("Bienvenido puede entrar")
+   
 
 if __name__ == '__main__':
     run()
