be104ac3458a60ae95b0e0809c12e64dc564bd34
Erick Escobar <edeajob27@gmail.com>
2022-08-23 17:03:53 -0500

    list comprehension

--- python_intermediate/repeated.py
+++ python_intermediate/repeated.py
@@ -5,15 +5,10 @@ def quit_repeated(my_list):
 
 
 def run():
-    elements = []
     elements_length = int(input(" How many elements? : "))
     if elements_length <= 0 :
         raise ValueError("You must provide at least one element")
-
-    for i in range(1,elements_length+1):
-        element = input(f" Type your {i} element for the  list: ")
-        elements.append(element)
-
+    elements = [input(f" Type your {i} element for the  list: ") for i in range(1,elements_length+1)]
     print("Original list = ",elements)
     print(f"List without repeated elements:{quit_repeated(elements)} ")
 if __name__ == '__main__':
