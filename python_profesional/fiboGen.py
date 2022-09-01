114a5503d6aeb9b664c5e6e1fd26993b719d266b
Erick Escobar <edeajob27@gmail.com>
2022-09-01 11:06:26 -0500

    el usuario da el numero

--- python_profesional/fiboGen.py
+++ python_profesional/fiboGen.py
@@ -12,13 +12,15 @@ def fibo_gen(n_max:int ) -> Generator:
         # en el segudo ciclo es 1
         # en el tercero es 0 +1 y asi para todos los demás iteraciones
         yield n1
-        # intercambiamos los valores en la sucesion 
+        # intercambiamos los valores en la sucesion
         n1, n2 = n2, n1 + n2
 
 if __name__ == '__main__':
-    # Creamos el objeto y le enviamos un numero maximo de 10 
+    # Creamos el objeto y le enviamos un numero maximo u_num
     #! La sucesion se detendra antes de llegar a 10 si queremos que se impriman 10 sucesiones debemos usar un ciclo for en fibo_gen
-    fibonacci: Generator = fibo_gen(10)
+    u_num:int = int(input("Please type a number to generate fibonacci sucesion: "))
+    assert u_num > 0,"Only positive numbers are allowed"
+    fibonacci: Generator = fibo_gen(u_num)
     for element in fibonacci:
         print(element)
         time.sleep(.5)
