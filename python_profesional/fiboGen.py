e1a05f3a5383d1a7869d0bd2991b6ce7ec1a71ae
Erick Escobar <edeajob27@gmail.com>
2022-09-01 10:59:21 -0500

    tipado estatico

--- python_profesional/fiboGen.py
+++ python_profesional/fiboGen.py
@@ -1,13 +1,24 @@
 import time
-
-def fibo_gen(n_max):
-    n1, n2 = 0, 1
+from typing import Generator
+def fibo_gen(n_max:int ) -> Generator:
+    """Generates a fibonacci sucesion until n_max is reached"""
+    # Se declaran las variables iniciales de la sucesion de fibonacci
+    n1:int = 0
+    n2:int = 1
+    # Si no ha llegado al maximo o si el maximo es mayor o igual que n1 el ciclo se ejecuta
     while not n_max or n_max >= n1:
+        # Retenemos el valor de n1 para que la funcion sea más rapida
+        # en el primer ciclo es 0
+        # en el segudo ciclo es 1
+        # en el tercero es 0 +1 y asi para todos los demás iteraciones
         yield n1
+        # intercambiamos los valores en la sucesion 
         n1, n2 = n2, n1 + n2
 
 if __name__ == '__main__':
-    fibonacci = fibo_gen(10)
+    # Creamos el objeto y le enviamos un numero maximo de 10 
+    #! La sucesion se detendra antes de llegar a 10 si queremos que se impriman 10 sucesiones debemos usar un ciclo for en fibo_gen
+    fibonacci: Generator = fibo_gen(10)
     for element in fibonacci:
         print(element)
         time.sleep(.5)
