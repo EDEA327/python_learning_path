f4e1fd6386f194b9c0a99309942e366375993605
Erick Escobar <edeajob27@gmail.com>
2022-09-01 08:44:31 -0500

    Ejemplos de clase

--- python_profesional/decorators.py
+++ python_profesional/decorators.py
@@ -1,6 +1,26 @@
-def execution_time():
-    pass
-def run():
-    pass
-if __name__ == '__main__':
-    run()
+from datetime import datetime
+
+def execution_time(func):
+    def wrapper(*args, **kwargs):
+        initial_time = datetime.now()
+        func(*args, **kwargs)
+        final_time = datetime.now()
+        time_elapsed = final_time - initial_time
+        print(f'Pasaron {time_elapsed.total_seconds()} segundos')
+    return wrapper
+
+
+@execution_time
+def random_func():
+    for _ in range(1, 10000):
+        pass
+@execution_time
+def suma( a: int , b: int ) -> int:
+    return a + b
+@execution_time
+def saludo(name: str = "Erick") -> str:
+    print(f"Hi {name}")
+
+random_func()
+suma(5,5)
+saludo()
